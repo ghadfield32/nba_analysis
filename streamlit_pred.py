c0c23c93c7efaa84ecf9fbd6dce5954c4eb91a19
ghadfield32 <ghadfield32@gmail.com>
2023-11-02 19:01:09 -0400

    added ltsm_seq and updated streamlit app so it is in order and has scatter plot chart to track progress

--- streamlit_pred.py
+++ streamlit_pred.py
@@ -31,11 +31,13 @@
 tree_pred_path = 'data/tree_season_pred.csv'
 non_tree_pred_path = 'data/non_tree_season_pred.csv'
 ltsm_pred_path = 'data/ltsm_season_pred.csv'
+ltsm_seq_pred_path = 'data/ltsm_seq_season_pred.csv'
 past_results_path = 'data/nba_threeptera_prepreprocess_data.csv'
 votes_data_path = 'data/voter_pred.csv'
 
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 from datetime import datetime
 
 # Data Loading Functions
@@ -49,6 +51,11 @@ def load_ltsm_data(path):
     ltsm_data = pd.read_csv(path)
     ltsm_data = ltsm_data.rename(columns={'PREDICTION': 'ltsm_PREDICTION'})
     return ltsm_data[['Date', 'MATCHUP_ID', 'TEAM_NAME', 'ltsm_PREDICTION']]
+
+def load_ltsm_seq_data(path):
+    ltsm_seq_data = pd.read_csv(path)
+    ltsm_seq_data = ltsm_seq_data.rename(columns={'PREDICTION': 'ltsm_seq_PREDICTION'})
+    return ltsm_seq_data[['Date', 'MATCHUP_ID', 'TEAM_NAME', 'ltsm_seq_PREDICTION']]
 
 def load_past_results(path):
     return pd.read_csv(path)
@@ -64,10 +71,17 @@ aggregated_votes = votes_data.groupby(['Date', 'MATCHUP_ID', 'TEAM_NAME']).sum()
 #print(aggregated_votes.head())
 
 # Merging function
-def merge_data(tree_data, non_tree_data, ltsm_data):
+def merge_data(tree_data, non_tree_data, ltsm_seq_data, ltsm_data):
     tree_non_tree = pd.merge(tree_data, non_tree_data, on=['Date', 'MATCHUP_ID', 'TEAM_NAME'], how='left')
-    all_data = pd.merge(tree_non_tree, ltsm_data, on=['Date', 'MATCHUP_ID', 'TEAM_NAME'], how='left')
+    tree_non_tree_ltsm = pd.merge(tree_non_tree, ltsm_seq_data, on=['Date', 'MATCHUP_ID', 'TEAM_NAME'], how='left')
+    all_data = pd.merge(tree_non_tree_ltsm, ltsm_data, on=['Date', 'MATCHUP_ID', 'TEAM_NAME'], how='left')
     return pd.merge(all_data, aggregated_votes, on=['Date', 'MATCHUP_ID', 'TEAM_NAME'], how='left')
+
+def calculate_daily_accuracy(data):
+    data['correct_prediction'] = data['ltsm_seq_PREDICTION'] == data['WL_encoded']
+    daily_accuracy = data.groupby('Date')['correct_prediction'].mean().reset_index()
+    daily_accuracy['Date'] = pd.to_datetime(daily_accuracy['Date'])
+    return daily_accuracy.sort_values(by='Date')
 
 def main():
     st.title("Man vs Machine: NBA Predictions")
@@ -76,13 +90,14 @@ def main():
     tree_data = load_tree_data(tree_pred_path)
     non_tree_data = load_non_tree_data(non_tree_pred_path)
     ltsm_data = load_ltsm_data(ltsm_pred_path)
-    all_data = merge_data(tree_data, non_tree_data, ltsm_data)
+    ltsm_seq_data = load_ltsm_seq_data(ltsm_seq_pred_path)
+    all_data = merge_data(tree_data, non_tree_data, ltsm_data, ltsm_seq_data)
     past_results = load_past_results(past_results_path)
     
     # Sidebar for navigation
-    app_mode = st.sidebar.selectbox("Choose the app mode", ["Man vs Machine", "All Predictions", "Past Results"])
+    app_mode = st.sidebar.selectbox("Choose the app mode", ["Man vs Machine", "All Predictions", "Voter Predictions"])
     
-    if app_mode == "Man vs Machine":
+    if app_mode == "Voter Predictions":
         st.subheader('Upcoming NBA Games (LSTM Predictions)')
         
         # Load previous votes if available
@@ -121,33 +136,78 @@ def main():
     
     elif app_mode == "All Predictions":
         st.subheader('All Predictions')
+        #sort by date and reset index
+        all_data = all_data.sort_values(by=['Date'], ascending=False)
+        all_data = all_data.reset_index(drop=True)
         st.write(all_data)
 
-    elif app_mode == "Past Results":
+    elif app_mode == "Man vs Machine":
         st.subheader('Past NBA Games and Predictions')
         past_results = past_results.rename(columns={'GAME_DATE': 'Date'})
         
         # Merge past results with predictions
         past_data_with_predictions = pd.merge(past_results, all_data, on=['Date', 'TEAM_NAME'], how='left')
 
-        #filter 
+        # Filter and sort data
         past_data_with_predictions = past_data_with_predictions[past_data_with_predictions['ltsm_PREDICTION'].notna()]
-        
+        past_data_with_predictions = past_data_with_predictions.sort_values(by=['Date', 'MATCHUP_ID_x'], ascending=False)
+        past_data_with_predictions = past_data_with_predictions.reset_index(drop=True)
+
+        # Only include the columns we want
+        past_data_with_predictions = past_data_with_predictions[['Date', 'MATCHUP', 'TEAM_NAME', 'WL', 'WL_encoded', 'ltsm_PREDICTION', 
+                                                                 'ltsm_seq_PREDICTION', 'voter_predictions']]
+        #print(past_data_with_predictions.head())
+
+        # Calculate daily accuracy for each prediction model
+        def calculate_daily_accuracy(data, prediction_column):
+            correct_predictions_column = f'{prediction_column}_correct'
+            data[correct_predictions_column] = data[prediction_column] == data['WL_encoded']
+            daily_accuracy = data.groupby('Date')[correct_predictions_column].mean().reset_index()
+            daily_accuracy['Date'] = pd.to_datetime(daily_accuracy['Date'])
+            daily_accuracy = daily_accuracy.sort_values(by='Date')
+            daily_accuracy['Model'] = prediction_column
+            daily_accuracy['correct_prediction'] = daily_accuracy[correct_predictions_column] * 100  # Convert to percentage
+            return daily_accuracy
+
+        lstm_accuracy = calculate_daily_accuracy(past_data_with_predictions, 'ltsm_PREDICTION')
+        lstm_seq_accuracy = calculate_daily_accuracy(past_data_with_predictions, 'ltsm_seq_PREDICTION')
+        voter_accuracy = calculate_daily_accuracy(past_data_with_predictions, 'voter_predictions')
+
+        # Combine accuracies into a single DataFrame
+        all_accuracies = pd.concat([lstm_accuracy, lstm_seq_accuracy, voter_accuracy])
+
+        # Filter for the last month's data
+        one_month_ago = datetime.now() - pd.to_timedelta(30, unit='d')
+        all_accuracies = all_accuracies[all_accuracies['Date'] > one_month_ago]
+
+        # Create line chart
+        fig = px.line(all_accuracies, x='Date', y='correct_prediction', color='Model', title='Daily Accuracy of Predictions', labels={'correct_prediction': 'Accuracy (%)'})
+        fig.update_yaxes(tickvals=[i for i in range(0, 101, 10)], ticktext=[f'{i}%' for i in range(0, 101, 10)])
+        st.plotly_chart(fig)
+
+
         # Calculate accuracy for LSTM predictions
         correct_lstm = sum(past_data_with_predictions['ltsm_PREDICTION'] == past_data_with_predictions['WL_encoded'])
         total_lstm = len(past_data_with_predictions['ltsm_PREDICTION'].dropna())
-        accuracy_lstm = correct_lstm / total_lstm * 100 if total_lstm != 0 else 0
-        
-        #change the -1's in voter_predictions to 0's
-        past_data_with_predictions['voter_predictions'] = past_data_with_predictions['voter_predictions'].replace(-1,0)
-        # Calculate accuracy for Voter predictions
+        accuracy_lstm = round((correct_lstm / total_lstm * 100) if total_lstm != 0 else 0, 2)
+
+        # Calculate accuracy for LSTM sequence predictions
+        correct_lstm_seq = sum(past_data_with_predictions['ltsm_seq_PREDICTION'] == past_data_with_predictions['WL_encoded'])
+        total_lstm_seq = len(past_data_with_predictions['ltsm_seq_PREDICTION'].dropna())
+        accuracy_lstm_seq = round((correct_lstm_seq / total_lstm_seq * 100) if total_lstm_seq != 0 else 0, 2)
+
+        # Handle voter predictions (-1's to 0's) and calculate accuracy
+        past_data_with_predictions['voter_predictions'] = past_data_with_predictions['voter_predictions'].replace(-1, 0)
         correct_voter = sum(past_data_with_predictions['voter_predictions'] == past_data_with_predictions['WL_encoded'])
         total_voter = len(past_data_with_predictions['voter_predictions'].dropna())
-        accuracy_voter = correct_voter / total_voter * 100 if total_voter != 0 else 0
+        accuracy_voter = round((correct_voter / total_voter * 100) if total_voter != 0 else 0, 2)
         
+        # Display accuracies
         st.write(f"LSTM Prediction Accuracy: {accuracy_lstm}%")
+        st.write(f"LSTM (5 Game Memory) Prediction Accuracy: {accuracy_lstm_seq}%")
         st.write(f"Voter Prediction Accuracy: {accuracy_voter}%")
         
+        # Display predictions and results
         st.write(past_data_with_predictions)
         
 if __name__ == "__main__":
