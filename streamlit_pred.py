edbdcaaefa3c5df2f8c31395bd2807039fef0c86
ghadfield32 <ghadfield32@gmail.com>
2023-12-14 13:54:31 -0500

    updated the predictions/models/data

--- streamlit_pred.py
+++ streamlit_pred.py
@@ -131,10 +131,10 @@ def calculate_daily_correct_percentage(data, model):
 
 #*******************************chatbot add on***********************************
 # Initialize session state for messages at the top level to ensure it's always done.
-#st.session_state.setdefault("messages", [{"role": "system", "content": "Warming up on the court! 🏀 Ready to assist and share some hoops wisdom. Pass the ball, and let's get this conversation rolling!"}])
+st.session_state.setdefault("messages", [{"role": "system", "content": "Warming up on the court! 🏀 Ready to assist and share some hoops wisdom. Pass the ball, and let's get this conversation rolling!"}])
 
 # Set the OpenAI API key.
-#openai.api_key = st.secrets["openai_key"]
+openai.api_key = st.secrets["openai_key"]
 
 
 # Function to generate initial prompt for the chatbot based on the model's predictions
@@ -348,10 +348,10 @@ def main():
         st.plotly_chart(fig)
 
         # Generate initial prompt for the chatbot
-        #initial_prompt = generate_initial_prompt(performance_df)
+        initial_prompt = generate_initial_prompt(performance_df)
 
         # Display the chatbot in the sidebar
-        #chatbot_sidebar(initial_prompt)
+        chatbot_sidebar(initial_prompt)
         
 
 # Run the main function
